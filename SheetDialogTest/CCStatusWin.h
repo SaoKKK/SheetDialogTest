1d4b6185ac4f02f14cd48e53f603bf6bccb67a5c
Saori Kohno <saorikohno@yahoo.com>
2016-02-13 17:18:24 +0900

    2/13 add status dialog

---
+++ SheetDialogTest/CCStatusWin.h
@@ -0,0 +1,13 @@
+//
+//  CCStatusWin.h
+//  SheetDialogTest
+//
+//  Created by 河野 さおり on 2014/07/30.
+//  Copyright (c) 2014年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CCStatusWin : NSWindow
+
+@end
